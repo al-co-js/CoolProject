825038b5703d89046135ffd77ca28196bd3d92cd
al-co-js <shimjs8@naver.com>
2020-12-09 09:34:49 +0900

    Initial commit

---
+++ src/main/java/com/coal/bond/CoolProject/CoolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.coal.bond.CoolProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolProjectApplication.class, args);
+	}
+
+}
