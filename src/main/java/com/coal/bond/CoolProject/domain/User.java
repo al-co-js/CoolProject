9238177462ed788e9068edaa3a1ae2d480398c89
al-co-js <shimjs8@naver.com>
2020-12-10 10:31:49 +0900

    feat: update jpa

--- src/main/java/com/coal/bond/CoolProject/domain/User.java
+++ src/main/java/com/coal/bond/CoolProject/domain/User.java
@@ -1,9 +1,27 @@
 package com.coal.bond.CoolProject.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    @Column(length = 100, nullable = false)
     String name;
+    @Column(nullable = false)
     String email;
+    @Column(nullable = false)
     String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
