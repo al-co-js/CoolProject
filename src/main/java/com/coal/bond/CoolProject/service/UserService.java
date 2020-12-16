fd15b43c4e5d1b8a90aa49fad208635d52bd6f5a
al-co-js <shimjs8@naver.com>
2020-12-16 09:08:43 +0900

    feat: home

--- src/main/java/com/coal/bond/CoolProject/service/UserService.java
+++ src/main/java/com/coal/bond/CoolProject/service/UserService.java
@@ -10,8 +10,12 @@ import java.util.Optional;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
     @Autowired
-    private UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public boolean create(User user) {
         try {
