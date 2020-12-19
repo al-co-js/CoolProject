f117f4c3c24f86979e9a14cdab88301870a9d64a
al-co-js <shimjs8@naver.com>
2020-12-20 00:26:34 +0900

    feat: many pages

--- src/main/java/com/coal/bond/CoolProject/controller/UserController.java
+++ src/main/java/com/coal/bond/CoolProject/controller/UserController.java
@@ -3,7 +3,9 @@ package com.coal.bond.CoolProject.controller;
 import com.coal.bond.CoolProject.domain.User;
 import com.coal.bond.CoolProject.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.coal.bond.CoolProject.form.UserForm;
@@ -11,6 +13,7 @@ import com.coal.bond.CoolProject.form.UserForm;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/user")
@@ -27,11 +30,11 @@ public class UserController {
     }
 
     @PostMapping("/signin")
-    public String login(UserForm uf, HttpServletResponse response) {
+    public String login(UserForm userForm, HttpServletResponse response) {
         List<User> list = userService.list();
-        for (User u : list) {
-            if (u.getEmail().equals(uf.getEmail()) && u.getPassword().equals(uf.getPassword())) {
-                Cookie cookie = new Cookie("id", u.getId().toString());
+        for (User user : list) {
+            if (user.getEmail().equals(userForm.getEmail()) && user.getPassword().equals(userForm.getPassword())) {
+                Cookie cookie = new Cookie("id", user.getId().toString());
                 cookie.setPath("/");
                 response.addCookie(cookie);
                 return "redirect:/";
@@ -62,5 +65,47 @@ public class UserController {
         cookie.setMaxAge(0);
         response.addCookie(cookie);
         return "redirect:/";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<User> users = userService.list();
+        model.addAttribute("users", users);
+        return "list";
+    }
+
+    @GetMapping("/profile/{id}")
+    public String profile(@PathVariable long id, Model model) {
+        Optional<User> user = userService.read(id);
+        if (user.isEmpty()) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("user", user.get());
+        return "profile";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String edit(@PathVariable long id, UserForm userForm) {
+        Optional<User> fetchedUser = userService.read(id);
+        if (fetchedUser.isEmpty()) {
+            return "redirect:/";
+        }
+
+        User user = fetchedUser.get();
+        if (!userForm.getName().equals("")) {
+            user.setName(userForm.getName());
+        }
+        if (!userForm.getEmail().equals("")) {
+            user.setEmail(userForm.getEmail());
+        }
+        userService.create(user);
+        return "redirect:/user/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String remove(@PathVariable long id) {
+        userService.delete(id);
+        return "redirect:/user/list";
     }
 }
