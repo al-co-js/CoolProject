6bb5baf87650e7d6a2729855a9cc2af4e7831dbd
al-co-js <shimjs8@naver.com>
2020-12-19 16:59:20 +0900

    refactor: remove unused import

--- src/main/java/com/coal/bond/CoolProject/controller/HomeController.java
+++ src/main/java/com/coal/bond/CoolProject/controller/HomeController.java
@@ -1,12 +1,8 @@
 package com.coal.bond.CoolProject.controller;
 
-import com.coal.bond.CoolProject.domain.User;
 import com.coal.bond.CoolProject.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.List;
 
 @Controller
 public class HomeController {
