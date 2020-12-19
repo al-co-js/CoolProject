a43b1f99dd61c022fd75959101a5a3e2674e9e3c
al-co-js <shimjs8@naver.com>
2020-12-19 18:41:24 +0900

    feat: sign out

--- src/main/java/com/coal/bond/CoolProject/controller/HomeController.java
+++ src/main/java/com/coal/bond/CoolProject/controller/HomeController.java
@@ -1,19 +1,18 @@
 package com.coal.bond.CoolProject.controller;
 
-import com.coal.bond.CoolProject.service.UserService;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    private UserService us;
-
-    public HomeController(UserService us) {
-        this.us = us;
-    }
-
     @GetMapping("/")
-    public String home() {
+    public String home(@CookieValue(name = "id", defaultValue = "null") String id, Model model) {
+        model.addAttribute("id", id);
         return "home";
     }
-    }
+}
--- src/main/java/com/coal/bond/CoolProject/controller/UserController.java
+++ src/main/java/com/coal/bond/CoolProject/controller/UserController.java
@@ -5,18 +5,20 @@ import com.coal.bond.CoolProject.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.coal.bond.CoolProject.form.UserForm;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
+    private UserService userService;
 
-    private UserService us;
-
-    public UserController(UserService us) {
-        this.us = us;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/signin")
@@ -26,10 +28,11 @@ public class UserController {
 
     @PostMapping("/signin")
     public String login(UserForm uf, HttpServletResponse response) {
-        List<User> list = us.list();
+        List<User> list = userService.list();
         for (User u : list) {
             if (u.getEmail().equals(uf.getEmail()) && u.getPassword().equals(uf.getPassword())) {
                 Cookie cookie = new Cookie("id", u.getId().toString());
+                cookie.setPath("/");
                 response.addCookie(cookie);
                 return "redirect:/";
             }
@@ -43,13 +46,21 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public String register(UserForm uf) {
-        User u = new User();
-        u.setName(uf.getName());
-        u.setEmail(uf.getEmail());
-        u.setPassword(uf.getPassword());
-        us.create(u);
+    public String register(UserForm userForm) {
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        userService.create(user);
         return "redirect:/";
     }
 
+    @GetMapping("/signout")
+    public String signout(HttpServletResponse response) {
+        Cookie cookie = new Cookie("id", null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return "redirect:/";
+    }
 }
